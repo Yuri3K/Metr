fa6f1341197ac852f97e95b48a727525be5641ed
Yuri_S <zebrawild@gmail.com>
2023-01-23 10:56:29 +0200

    added some untreaked files

--- src/js/singleItemPage/singleItemController.js
+++ src/js/singleItemPage/singleItemController.js
@@ -1,9 +1,25 @@
 import SingleItem from './../singleItemPage/singleItemModel';
-import * as view from './../singleItemPage/sungleItemView';
+import * as view from './singleItemView';
 
 export default async function (state) {
   state.singleItem = new SingleItem(state.routeParams);
   await state.singleItem.getItem();
   view.render(state.singleItem.result);
-  console.log('state', state.singleItem.result)
+  console.log('state', state.singleItem.result);
+
+  document.querySelector('.button-order').addEventListener('click', () => {
+    view.showModal();
+  })
+
+  document.querySelector('.modal__close').addEventListener('click', () => {
+    view.hideModal();
+  })
+
+  document.querySelector('.modal-wrapper').addEventListener('click', (event) => {
+    if (event.target.closest('.modal')) {
+      return
+    } else {
+      view.hideModal();
+    }
+  })
 }
--- src/js/singleItemPage/singleItemView.js
+++ src/js/singleItemPage/singleItemView.js
@@ -1,5 +1,5 @@
 export function render(object) {
-  const markup = `<div class="container p-0">
+    const markup = `<div class="container p-0">
                     <div class="heading-1">
                         ${object.title}, ${object.square} м2 за ${object.price_total} ₽
                     </div>
@@ -92,5 +92,85 @@ export function render(object) {
                     <a href="/" class="back-to-results"
                         >← Вернуться к результатам поиска</a>
                   </div>`;
-  document.querySelector('#app').insertAdjacentHTML("beforeend", markup)
+
+    const markupModal = `<div class="modal-wrapper none">
+                            <div class="modal">
+                                <div class="modal__header">
+                                    <div class="modal__title">
+                                        Заявка на бронирование
+                                    </div>
+                                    <div class="modal__details">
+                                        Квартира <span>96</span> в Первом квартале Дом 5
+                                        <div class="modal__details-art">ГЕН-112-42</div>
+                                    </div>
+                                </div>
+
+                                <form class="modal__form">
+                                    <div class="modal__form-content">
+                                        <!-- formgroup -->
+                                        <div class="formgroup">
+                                            <label
+                                                class="modal__form-input-label"
+                                                for="form-phone"
+                                            >
+                                                Имя
+                                            </label>
+                                            <input
+                                                type="text"
+                                                id="form-name"
+                                                class="modal__form-input"
+                                                placeholder="Введите имя"
+                                            />
+                                        </div>
+                                        <!-- // formgroup -->
+                                        <!-- formgroup -->
+                                        <div class="formgroup">
+                                            <label
+                                                class="modal__form-input-label"
+                                                for="form-phone"
+                                            >
+                                                Телефон
+                                            </label>
+                                            <input
+                                                type="text"
+                                                id="form-phone"
+                                                class="modal__form-input"
+                                                placeholder="+7 (XXX) XXX-XX-XX"
+                                            />
+                                        </div>
+                                        <!-- // formgroup -->
+
+                                        <div class="formgroup formgroup--checkbox">
+                                            <input type="checkbox" id="policy" checked />
+                                            <label class="policy-text" for="policy"
+                                                >Я согласен на обработку моих персональных
+                                                данных. С Политикой в отношении обработки
+                                                персональных данных ознакомлен и
+                                                согласен.</label
+                                            >
+                                        </div>
+                                    </div>
+                                    <input
+                                        class="modal__submit"
+                                        type="submit"
+                                        value="Отправить заявку"
+                                    />
+                                </form>
+                                <button class="modal__close">
+                                    Закрыть
+                                </button>
+                            </div>
+                        </div>`;
+
+
+    document.querySelector('#app').insertAdjacentHTML("beforeend", markup)
+    document.querySelector('#app').insertAdjacentHTML("beforeend", markupModal)
+}
+
+export function showModal() {
+    document.querySelector('.modal-wrapper').classList.remove('none')
+}
+
+export function hideModal() {
+    document.querySelector('.modal-wrapper').classList.add('none')
 }
